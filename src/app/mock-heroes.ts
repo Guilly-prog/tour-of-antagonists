e5c0bee1d06d79e6a5e3d163338bdc39d79b2cd6
Guilherme <glopes865@gmail.com>
2021-07-30 12:21:09 -0300

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Loira do tchan' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Padre marcelo rocci' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Maria do bairro' },
+  { id: 16, name: 'RubberMan o melhor borracheiro da cidade' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ... IQ QUE EU TENHO AVE COM IÇU?' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
